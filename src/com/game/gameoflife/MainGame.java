442a84d47d2c2aeaa30fc94db49295778b3a5230
Julien <julien.colin@epitech.eu>
2014-10-14 16:01:18 +0200

    Initial commit :+1:

---
+++ src/com/game/gameoflife/MainGame.java
@@ -0,0 +1,15 @@
+package com.game.gameoflife;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainGame extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_game);
+	}
+}
